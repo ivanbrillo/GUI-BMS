6b189dd597e4ba363dc4ff7ea48a8b489f0c8191
ivanbrillo <ivan.brillo@raceup.it>
2023-07-03 18:17:30 +0200

    Serial controller finished

---
+++ src/Constants.py
@@ -0,0 +1,45 @@
+import struct
+
+N_VS = 9
+N_TS = 3
+N_SLAVES = 16
+
+UPDATE_FREQ = 300
+MIN_ERR = 30
+
+# H -> half_word (2 Byte),  ? -> bool (1 Byte),  c -> char (1 Byte), I -> Unsigned int  B -> uint8, x -> pad byte
+# https://docs.python.org/3/library/struct.html
+# pay attention to the struct alignment in Host code!
+
+# uint16_t volts[]; int16_t temps[];  uint8_t addr;  uint8_t err;
+FORMAT_SLAVE = "H" * (N_VS + N_TS) + "BB"
+size_slave = struct.calcsize(FORMAT_SLAVE)
+
+# uint16_t max_volt;  uint16_t min_volt;  uint32_t tot_volt;  uint16_t max_temp; uint16_t min_temp;  uint16_t tot_temp;  uint8_t max_temp_slave;
+FORMAT_MIN_MAX = "H" * 2 + "I" + "H" * 3 + "Bx"
+size_minmax = struct.calcsize(FORMAT_MIN_MAX)
+
+# uint16_t prev_temp; bool on;
+FORMAT_FAN = "H?x"
+size_fan = struct.calcsize(FORMAT_FAN)
+
+# uint32_t curr;  uint32_t last_recv;
+FORMAT_LEM = "I" * 2
+size_lem = struct.calcsize(FORMAT_LEM)
+
+#   bool sdc_closed;  uint32_t fault_volt_tmstp;  uint32_t fault_temp_tmstp;  Mode mode (int 32 bit);
+FORMAT_ADDITIONAL_INFO = "?xxx" + "I" * 2 + "i"
+size_additional_info = struct.calcsize(FORMAT_ADDITIONAL_INFO)
+
+#   float bus_volt;  bool via_can;  uint32_t start_tmstp;  uint8_t cycle_counter;  bool done;
+FORMAT_PRECHARGE = "f?xxxIB?xx"
+size_precharge = struct.calcsize(FORMAT_PRECHARGE)
+
+FORMAT_PAYLOAD = FORMAT_SLAVE * N_SLAVES + FORMAT_MIN_MAX + FORMAT_FAN + FORMAT_LEM + FORMAT_ADDITIONAL_INFO + FORMAT_PRECHARGE + "?xxx"  # +computer connected and padding
+size_payload = struct.calcsize(FORMAT_PAYLOAD)
+
+print("SLAVE:" + str(size_slave))
+print("PRECHARGE:" + str(struct.calcsize(FORMAT_PRECHARGE)))
+print("LEM:" + str(size_lem))
+print("ADD:" + str(struct.calcsize(FORMAT_ADDITIONAL_INFO)))
+print("TOT:" + str(size_payload))
---
+++ src/SerialController.py
@@ -0,0 +1,67 @@
+import struct
+import time
+from Constants import *
+import serial.tools.list_ports
+
+
+# static method to get all the available (connected) serial port at that moment
+def get_com() -> list:
+    return [port.device for port in serial.tools.list_ports.comports()]
+
+
+class SerialController:
+    def __init__(self):
+        self.ser = serial.Serial()
+
+    def open(self, baud_rate: int, selected_com: str):
+        self.ser.baudrate = baud_rate
+        self.ser.port = selected_com
+        self.ser.open()
+
+        time.sleep(0.4)
+        self.set_mode("C")
+
+    # send the selected mode to the host
+    def set_mode(self, mode: str):
+        self.ser.write(bytes(mode, 'utf-8'))
+        self.ser.flush()
+
+    def close(self):
+        self.set_mode("D")
+        self.ser.close()
+
+    def _check_timeout(self, time_out):
+        if time.time() > time_out:
+            self.close()
+            raise TimeoutError
+
+    def _read(self, size: int) -> bytes:
+
+        time_out = time.time() + 2
+        while self.ser.in_waiting < size and time.time() < time_out:
+            pass
+
+        self._check_timeout(time_out)
+        return bytes(self.ser.read(size))
+
+    # Read the start sequence and only then read the payload sent by the BMS. There's a time out in order to avoid
+    # infinite loop on a wrong com
+    def read_packet(self) -> bytes:
+        read = False
+        time_out = time.time() + 2
+        bytes_buffer = bytearray(4)
+
+        while not read and time.time() < time_out:
+            bytes_buffer.append(self._read(1))
+            bytes_buffer.pop(0)
+
+            if bytes_buffer[-4:] == b'\xff\xff\xff\xff':
+                read = True
+
+        self._check_timeout(time_out)
+        resp = self._read(size_payload)
+
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
+
+        return resp
